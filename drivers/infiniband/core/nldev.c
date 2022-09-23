fc2a5c4a349468a38213b607a881733defbbfd58
Zhu Yanjun <yanjun.zhu@linux.dev>
2022-09-23 16:52:45 +0000

    RDMA/nldev: Add dellink function pointer
    The newlink function pointer is added. And the sock listening on port 4791
    is added in the newlink function. So the dellink function is needed to
    remove the sock.
    
    Tested-by: Rain River <rain.1986.08.12@gmail.com>
    Signed-off-by: Zhu Yanjun <yanjun.zhu@linux.dev>

--- drivers/infiniband/core/nldev.c
+++ drivers/infiniband/core/nldev.c
@@ -1758,6 +1758,12 @@ static int nldev_dellink(struct sk_buff *skb, struct nlmsghdr *nlh,
 		return -EINVAL;
 	}
 
+	if (device->link_ops && device->link_ops->dellink) {
+		err = device->link_ops->dellink(device);
+		if (err)
+			return err;
+	}
+
 	ib_unregister_device_and_put(device);
 	return 0;
 }
