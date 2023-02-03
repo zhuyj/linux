7a5ace999c7fe501963b9c8feefa096446d99b4b
Zhu Yanjun <yanjun.zhu@linux.dev>
2023-02-03 14:30:57 +0000

    RDMA/rxe: add the support of net namespace
    Originally init_net is used to indicate the current net namespace.
    Currently more net namespaces are supported.
    
    Signed-off-by: Zhu Yanjun <yanjun.zhu@linux.dev>

--- drivers/infiniband/sw/rxe/rxe_net.h
+++ drivers/infiniband/sw/rxe/rxe_net.h
@@ -15,7 +15,7 @@ int rxe_net_add(const char *ibdev_name, struct net_device *ndev);
 void rxe_net_del(struct ib_device *dev);
 
 int rxe_register_notifier(void);
-int rxe_net_init(void);
+int rxe_net_init(struct net_device *ndev);
 void rxe_net_exit(void);
 
 #endif /* RXE_NET_H */
