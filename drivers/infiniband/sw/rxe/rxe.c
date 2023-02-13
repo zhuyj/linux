0c32ed6c316eff62fb8a461cbb099d303b31cb97
Zhu Yanjun <yanjun.zhu@linux.dev>
2023-02-13 17:05:44 +0000

    Revert "RDMA/rxe: Implement dellink in rxe"
    This reverts commit 1831a3b38c3b0815deea546eb6462fe9158e0eb6.

--- drivers/infiniband/sw/rxe/rxe.c
+++ drivers/infiniband/sw/rxe/rxe.c
@@ -166,12 +166,10 @@ void rxe_set_mtu(struct rxe_dev *rxe, unsigned int ndev_mtu)
 /* called by ifc layer to create new rxe device.
  * The caller should allocate memory for rxe by calling ib_alloc_device.
  */
-static struct rdma_link_ops rxe_link_ops;
 int rxe_add(struct rxe_dev *rxe, unsigned int mtu, const char *ibdev_name)
 {
 	rxe_init(rxe);
 	rxe_set_mtu(rxe, mtu);
-	rxe->ib_dev.link_ops = &rxe_link_ops;
 
 	return rxe_register_device(rxe, ibdev_name);
 }
@@ -208,17 +206,9 @@ err:
 	return err;
 }
 
-static int rxe_dellink(struct ib_device *dev)
-{
-	rxe_net_del(dev);
-
-	return 0;
-}
-
-static struct rdma_link_ops rxe_link_ops = {
+struct rdma_link_ops rxe_link_ops = {
 	.type = "rxe",
 	.newlink = rxe_newlink,
-	.dellink = rxe_dellink,
 };
 
 static int __init rxe_module_init(void)
