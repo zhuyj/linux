ffe38932e215bf6dee2d603dc274f625ee8c8bde
Zhu Yanjun <yanjun.zhu@linux.dev>
2023-03-06 06:15:55 -0500

    idpf: Remove kcompat.c

--- drivers/net/ethernet/intel/idpf/idpf/kcompat.h
+++ drivers/net/ethernet/intel/idpf/idpf/kcompat.h
@@ -2,33 +2,4 @@
 /* Copyright (C) 2019 Intel Corporation */
 #ifndef _KCOMPAT_H_
 #define _KCOMPAT_H_
-
-#include "kcompat_gcc.h"
-#include <linux/version.h>
-#include <linux/io.h>
-#include <linux/delay.h>
-#include <linux/errno.h>
-#include <linux/etherdevice.h>
-#include <linux/ethtool.h>
-#include <linux/if_vlan.h>
-#include <linux/in.h>
-#include <linux/if_link.h>
-#include <linux/init.h>
-#include <linux/ioport.h>
-#include <linux/ip.h>
-#include <linux/ipv6.h>
-#include <linux/list.h>
-#include <linux/mii.h>
-#include <linux/module.h>
-#include <linux/netdevice.h>
-#include <linux/pci.h>
-#include <linux/sched.h>
-#include <linux/skbuff.h>
-#include <linux/slab.h>
-#include <linux/string.h>
-#include <linux/tcp.h>
-#include <linux/types.h>
-#include <linux/udp.h>
-#include <linux/vmalloc.h>
-
 #endif /* _KCOMPAT_H_ */
