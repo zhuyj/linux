7a7bf902913980c22e498648c2bb3c873eb6ef06
Zhu Yanjun <yanjun.zhu@linux.dev>
2023-10-05 18:08:30 +0800

    RDMA/rxe: Support more rdma links in init_net
    In init_net, when several rdma links are created with the command "rdma
    link add", newlink will check whether the udp port 4791 is listening or
    not.
    If not, creating a sock listening on udp port 4791. If yes, increasing the
    reference count of the sock.
    
    Signed-off-by: Zhu Yanjun <yanjun.zhu@linux.dev>

--- drivers/infiniband/sw/rxe/rxe.c
+++ drivers/infiniband/sw/rxe/rxe.c
@@ -8,6 +8,7 @@
 #include <net/addrconf.h>
 #include "rxe.h"
 #include "rxe_loc.h"
+#include "rxe_net.h"
 
 MODULE_AUTHOR("Bob Pearson, Frank Zago, John Groves, Kamal Heib");
 MODULE_DESCRIPTION("Soft RDMA transport");
@@ -235,7 +236,7 @@ err:
 	return err;
 }
 
-static struct rdma_link_ops rxe_link_ops = {
+struct rdma_link_ops rxe_link_ops = {
 	.type = "rxe",
 	.newlink = rxe_newlink,
 };
@@ -249,6 +250,13 @@ static int __init rxe_module_init(void)
 		return err;
 
 	rdma_link_register(&rxe_link_ops);
+	err = rxe_register_notifier();
+	if (err) {
+		pr_err("Failed to register netdev notifier\n");
+		rxe_destroy_wq();
+		return -1;
+	}
+
 	pr_info("loaded\n");
 	return 0;
 }
