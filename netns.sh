6baeb877db171571109b022107659d445b5f1906
Zhu Yanjun <yanjun.zhu@linux.dev>
2023-10-10 22:21:46 -0400

    netns.sh: Add sleep time

--- netns.sh
+++ netns.sh
@@ -1,15 +1,20 @@
+set -x
 ip netns add test1
 ip netns ls
+sleep 2
 ip link add veth-a type veth peer name veth-b
 ip l
+sleep 2
 ip link set veth-a netns test1
 ip l
+sleep 2
 ip netns exec test1 ip l set veth-a up
 ip netns exec test1 ip addr add 1.1.1.1/24 dev veth-a
 ip netns exec test1 ip l
+sleep 2
 ip netns exec test1 ip -4 a
+sleep 2
 ip netns exec test1 rdma link add rxe0 type rxe netdev veth-a
-rdma link
 ip netns exec test1 rdma link
 ip netns exec test1 rping -s -a 1.1.1.1&
 ip link set veth-b up
@@ -20,3 +25,4 @@ rping -c -a 1.1.1.1 -d -v -C 3
 ip netns ls
 ip netns del test1
 ip netns ls
+set +x
