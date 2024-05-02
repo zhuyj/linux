bb88190c4553ef53d3f106b971bedb2f5367b3e2
Zhu Yanjun <yanjun.zhu@linux.dev>
2024-05-01 22:50:22 -0400

    RDMA/rxe: Replace netdev dev addr with raw_gid
    Because TUN device does not have dev_addr, but a gid in rdma is needed,
    as such, a raw_gid is generated to act as the gid. The similar commit is
    in SIW. This commit learns from the similar commit bad5b6e34ffb
    ("RDMA/siw: Fabricate a GID on tun and loopback devices") in SIW.
    
    Signed-off-by: Zhu Yanjun <yanjun.zhu@linux.dev>

--- drivers/infiniband/sw/rxe/rxe.c
+++ drivers/infiniband/sw/rxe/rxe.c
@@ -72,8 +72,20 @@ static void rxe_init_device_param(struct rxe_dev *rxe)
 	rxe->attr.max_fast_reg_page_list_len	= RXE_MAX_FMR_PAGE_LIST_LEN;
 	rxe->attr.max_pkeys			= RXE_MAX_PKEYS;
 	rxe->attr.local_ca_ack_delay		= RXE_LOCAL_CA_ACK_DELAY;
+
+	if (rxe->ndev->addr_len) {
+		memcpy(rxe->raw_gid, rxe->ndev->dev_addr,
+			min_t(unsigned int, rxe->ndev->addr_len, ETH_ALEN));
+	} else {
+		/*
+		 * This device does not have a HW address, but
+		 * connection mangagement requires a unique gid.
+		 */
+		eth_random_addr(rxe->raw_gid);
+	}
+
 	addrconf_addr_eui48((unsigned char *)&rxe->attr.sys_image_guid,
-			rxe->ndev->dev_addr);
+			rxe->raw_gid);
 
 	rxe->max_ucontext			= RXE_MAX_UCONTEXT;
 }
@@ -113,7 +125,7 @@ static void rxe_init_ports(struct rxe_dev *rxe)
 
 	rxe_init_port_param(port);
 	addrconf_addr_eui48((unsigned char *)&port->port_guid,
-			    rxe->ndev->dev_addr);
+			    rxe->raw_gid);
 	spin_lock_init(&port->port_lock);
 }
 
