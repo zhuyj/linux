cf9c3b548cc076fe4cdf66ee4d6ddecd835613f8
Zhu Yanjun <yanjun.zhu@linux.dev>
2024-05-20 11:34:31 +0200

    RDMA/srq: Add enable srq flag in rdma_client

--- srq_example/rdma_client.c
+++ srq_example/rdma_client.c
@@ -35,9 +35,11 @@
 #include <getopt.h>
 #include <rdma/rdma_cma.h>
 #include <rdma/rdma_verbs.h>
+#include <stdbool.h>
 
 static const char *server = "127.0.0.1";
 static const char *port = "7471";
+static bool enable_srq = false;
 
 static struct rdma_cm_id *id;
 static struct ibv_mr *mr, *send_mr;
@@ -143,7 +145,7 @@ int main(int argc, char **argv)
 {
 	int op, ret;
 
-	while ((op = getopt(argc, argv, "s:p:")) != -1) {
+	while ((op = getopt(argc, argv, "s:p:e:")) != -1) {
 		switch (op) {
 		case 's':
 			server = optarg;
@@ -151,10 +153,14 @@ int main(int argc, char **argv)
 		case 'p':
 			port = optarg;
 			break;
+		case 'e':
+			enable_srq = true;
+			break;
 		default:
 			printf("usage: %s\n", argv[0]);
 			printf("\t[-s server_address]\n");
 			printf("\t[-p port_number]\n");
+			printf("\t[-e enable srq]\n");
 			exit(1);
 		}
 	}
