409e72ca49ed9b571fcfa9d7af5753822eb558af
Zhu Yanjun <yanjun.zhu@linux.dev>
2024-05-21 12:01:38 +0200

    RDMA/srq: Add rq srq notifications

--- srq_example/test.sh
+++ srq_example/test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 CUR_DIR=`pwd`
 killall -9 rdma_server
+echo "RQ tests"
 ${CUR_DIR}/rdma_server -s 1.1.1.1 &
 sleep 3
 ${CUR_DIR}/rdma_client -s 1.1.1.1
 killall -9 rdma_server
+echo "SRQ tests"
 ${CUR_DIR}/rdma_server -s 1.1.1.1 -e &
 sleep 3
 ${CUR_DIR}/rdma_client -s 1.1.1.1 -e
