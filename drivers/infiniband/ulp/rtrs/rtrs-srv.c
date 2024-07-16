2e5de6275f99fd541bc35f42145f207daeae843d
Zhu Yanjun <yanjun.zhu@linux.dev>
2024-07-16 20:59:31 +0200

    RDMA/rtrs-srv: add ib_poll_cq to free cq
    Add ib_poll_cq to free cqe in rq and srq.
    
    Signed-off-by: Zhu Yanjun <yanjun.zhu@linux.dev>

--- drivers/infiniband/ulp/rtrs/rtrs-srv.c
+++ drivers/infiniband/ulp/rtrs/rtrs-srv.c
@@ -1231,6 +1231,13 @@ static void rtrs_srv_rdma_done(struct ib_cq *cq, struct ib_wc *wc)
 		return;
 	}
 
+	if (cq->device->ops.poll_cq) {
+		struct ib_wc wc_poll = {};
+
+		while (ib_poll_cq(cq, 16, &wc_poll) == 16)
+			;
+	}
+
 	switch (wc->opcode) {
 	case IB_WC_RECV_RDMA_WITH_IMM:
 		/*
