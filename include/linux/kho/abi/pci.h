fac248b53ac1447f47fabde98c4c29ed1629b8e0
David Matlack <dmatlack@google.com>
2026-04-15 18:43:44 +0000

    PCI: liveupdate: Set up FLB handler for the PCI core
    Set up a File-Lifecycle-Bound (FLB) handler for the PCI core to enable
    it to participate in the preservation of PCI devices across Live Update.
    Essentially, this commit enables the PCI core to allocate a struct
    (struct pci_ser) and preserve it across a Live Update whenever at least
    one device is preserved.
    
    Preserving PCI devices across Live Update is built on top of the Live
    Update Orchestrator's (LUO) support for file preservation. Drivers are
    expected to expose a file to userspace to represent a single PCI device
    and support preservation of that file. This is intended primarily to
    support preservation of PCI devices bound to VFIO drivers.
    
    This commit enables drivers to register their liveupdate_file_handler
    with the PCI core so that the PCI core can do its own tracking and
    enforcement of which devices are preserved.
    
      pci_liveupdate_register_flb(driver_file_handler);
      pci_liveupdate_unregister_flb(driver_file_handler);
    
    When the first file (with a handler registered with the PCI core) is
    preserved, the PCI core will be notified to allocate its tracking struct
    (pci_ser). When the last file is unpreserved (i.e. preservation
    cancelled) the PCI core will be notified to free struct pci_ser.
    
    This struct is preserved across a Live Update using KHO and can be
    fetched by the PCI core during early boot (e.g. during device
    enumeration) so that it knows which devices were preserved.
    
    Note: This commit only allocates struct pci_ser and preserves it across
    Live Update. A subsequent commit will add an API for drivers to tell the
    PCI core exactly which devices are being preserved.
    
    Note: There is no reason to check for kho_is_enabled() since it can be
    assumed to return true. If KHO was not enabled then Live Update would
    not be enabled and these routines would never run.
    
    Reviewed-by: Pranjal Shrivastava <praan@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>

---
+++ include/linux/kho/abi/pci.h
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2026, Google LLC.
+ * David Matlack <dmatlack@google.com>
+ */
+
+#ifndef _LINUX_KHO_ABI_PCI_H
+#define _LINUX_KHO_ABI_PCI_H
+
+#include <linux/bug.h>
+#include <linux/compiler.h>
+#include <linux/types.h>
+
+/**
+ * DOC: PCI File-Lifecycle Bound (FLB) Live Update ABI
+ *
+ * This header defines the ABI for preserving core PCI state across kexec using
+ * Live Update File-Lifecycle Bound (FLB) data.
+ *
+ * This interface is a contract. Any modification to any of the serialization
+ * structs defined here constitutes a breaking change. Such changes require
+ * incrementing the version number in the PCI_LUO_FLB_COMPATIBLE string.
+ */
+
+#define PCI_LUO_FLB_COMPATIBLE "pci-v1"
+
+/**
+ * struct pci_dev_ser - Serialized state about a single PCI device.
+ *
+ * @domain: The device's PCI domain number (segment).
+ * @bdf: The device's PCI bus, device, and function number.
+ * @refcount: Reference count used by the PCI core to keep track of whether it
+ *            is done using a device's struct pci_dev_ser. The value of the
+ *            refcount is equal to 1 when the struct pci_dev_ser is in use, and
+ *            0 otherwise.
+ */
+struct pci_dev_ser {
+	u32 domain;
+	u16 bdf;
+	u16 refcount;
+} __packed;
+
+/**
+ * struct pci_ser - PCI Subsystem Live Update State
+ *
+ * This struct tracks state about all devices that are being preserved across
+ * a Live Update for the next kernel.
+ *
+ * @nr_devices: The number of devices that were preserved.
+ * @devices: Physical address of the first KHO block containing pci_dev_ser.
+ */
+struct pci_ser {
+	u32 nr_devices;
+	u64 devices;
+} __packed;
+
+#endif /* _LINUX_KHO_ABI_PCI_H */
